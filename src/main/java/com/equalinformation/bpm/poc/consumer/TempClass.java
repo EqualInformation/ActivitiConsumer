9502436dcc8f7500c32d21af68cd34d70098bef1
bpupadhyaya <bpupadhyaya@gmail.com>
2015-11-02 12:47:22 -0800

    Testing repo. Delete this class later.

---
+++ src/main/java/com/equalinformation/bpm/poc/consumer/TempClass.java
@@ -0,0 +1,10 @@
+package com.equalinformation.bpm.poc.consumer;
+
+/**
+ * Created by bpupadhyaya on 11/2/15.
+ */
+public class TempClass {
+    public static void main(String...args) {
+        System.out.println("this is a repo test, delete later!");
+    }
+}
