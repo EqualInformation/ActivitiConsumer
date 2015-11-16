af7c872cb047d088c1796134e29867d09c576d17
bpupadhyaya <bpupadhyaya@gmail.com>
2015-11-16 15:25:40 -0800

    Activiti REST service client test added. Able to make successful REST call. Other files are in progress, just placeholders for now.

---
+++ src/main/java/com/equalinformation/bpm/poc/consumer/ws/ActivitiRESTClient.java
@@ -0,0 +1,14 @@
+package com.equalinformation.bpm.poc.consumer.ws;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+/**
+ * Created by bpupadhyaya on 11/16/15.
+ */
+public class ActivitiRESTClient {
+
+
+
+}
---
+++ src/main/java/com/equalinformation/bpm/poc/consumer/ws/domain/TaskResponse.java
@@ -0,0 +1,7 @@
+package com.equalinformation.bpm.poc.consumer.ws.domain;
+
+/**
+ * Created by bpupadhyaya on 11/16/15.
+ */
+public class TaskResponse {
+}
